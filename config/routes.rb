348fb86ac42343b7317b04a8a2899fe7a0e40bcf
Moirted <moirted@gmail.com>
2022-12-28 19:57:04 +0300

    Login/Registration realization. Header changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root 'static#index'
+  root 'static#index'
+
+
 end
